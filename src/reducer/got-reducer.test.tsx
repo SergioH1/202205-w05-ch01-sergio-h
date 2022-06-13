3ba61b7b5e0ef989e551fd225a8c6bb7f6a8f2c0
Sergio H <sergiohernandezroman@gmail.com>
2022-06-13 23:28:03 +0200

    add testing reducer

--- src/reducer/got-reducer.test.tsx
+++ src/reducer/got-reducer.test.tsx
@@ -1,5 +1,61 @@
-describe('Given the provider redux', () => {
-    describe('when is called', () => {
-        test('should first', () => {});
+import { gotReducer } from './got-reducer';
+import * as actions from './action-creator';
+import { gotModel } from '../models/got-model';
+
+const mockedArray: Array<gotModel> = [
+    {
+        id: 0,
+        name: '',
+        isDead: false,
+
+        family: '',
+    },
+    {
+        id: 1,
+        name: '',
+        isDead: true,
+
+        family: '',
+    },
+];
+describe('Given gots reducer', () => {
+    describe('When calling it with load action', () => {
+        test('It should load the mocked array of gots', () => {
+            const newState = gotReducer([], actions.loadGotAction(mockedArray));
+            expect(newState).toEqual(mockedArray);
+        });
+    });
+    describe('When calling it with add action', () => {
+        test('It should return an array with the mocked got', () => {
+            const newState = gotReducer(
+                [],
+                actions.addGotAction(mockedArray[0])
+            );
+            expect(newState).toEqual([mockedArray[0]]);
+        });
+    });
+    describe('When calling it with update action', () => {
+        test('It should load the mocked array of gots', () => {
+            const mockedName = 'test 1';
+            const newState = gotReducer(
+                mockedArray,
+                actions.updateGotAction({
+                    ...mockedArray[0],
+                    name: mockedName,
+                })
+            );
+            expect(newState.find((item) => item.id === 0)!.name).toEqual(
+                mockedName
+            );
+        });
+    });
+    describe('When calling it with delete action', () => {
+        test('It should load the mocked array of gots without the deleted one', () => {
+            const newState = gotReducer(
+                mockedArray,
+                actions.deleteGotAction(mockedArray[0])
+            );
+            expect(newState).toEqual([mockedArray[1]]);
+        });
     });
 });
--- src/reducer/got-reducer.ts
+++ src/reducer/got-reducer.ts
@@ -2,7 +2,7 @@ import { createReducer } from '@reduxjs/toolkit';
 import { gotModel } from '../models/got-model';
 import * as ac from './action-creator';
 const initialState: Array<gotModel> = [];
-export const taskReducer = createReducer(initialState, (builder) => {
+export const gotReducer = createReducer(initialState, (builder) => {
     builder
         .addCase(ac.loadGotAction, (state, action) => [...action.payload])
         .addCase(ac.addGotAction, (state, action) => [...state, action.payload])
